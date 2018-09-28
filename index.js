a7dc2c3da0ef583827a8a72e9bc3e58a4395ff00
patni1992 <patriknilsson1992@gmail.com>
2018-09-28 17:00:27 +0200

    test

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log('hello'.green); // outputs green text
 console.log('i like cake and pies'.underline.red) // outputs red underlined text
 console.log('inverse the color'.inverse); // inverses the color
 console.log('OMG Rainbows!'.rainbow); // rainbow
-console.log('Run th trap'.trap); // Drops the bass
+console.log('Run th trap'.trap); // Drops thed bass
