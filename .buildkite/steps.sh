d1fdcee6e8c36f126934a44126af013c75fc58a1
patni1992 <patriknilsson1992@gmail.com>
2018-09-28 19:21:25 +0200

    test

--- .buildkite/steps.sh
+++ .buildkite/steps.sh
@@ -4,13 +4,10 @@ set -uo pipefail
 yarn install 
 echo "installing dependecies"
  
- 
+ echo "linting" 
  yarn run lint
- echo "linting"
 
- TESTS_EXIT_STATUS=$?
 
 echo "cleaning"
  yarn install --production --ignore-scripts --prefer-offline
 
- exit $TESTS_EXIT_STATUS
