97d251fcfb7df6117d0e8c3be213dfac1aa0a843
patni1992 <patriknilsson1992@gmail.com>
2018-09-28 18:58:39 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
-var colors = require('colors');
-console.log('hello'.green); // outputs green text
-console.log('i like cake and pies'.underline.red) // outputs red underlined text
-console.log('inverse the color'.inverse); // inverses the color
-console.log('OMG Rainbows!'.rainbow); // rainbow
-console.log('Run th trap'.trap); // Drops thed bass
+var colors = require("colors");
+console.log("hello".green); // outputs green text
+console.log("i like cake and pies".underline.red); // outputs red underlined text
+console.log("inverse the color".inverse); // inverses the color
+console.log("OMG Rainbows!".rainbow); // rainbow
+console.log("Run th trap".trap); // Drops thed bass
+console.log(colors);
